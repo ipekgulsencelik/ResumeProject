bb85f94d3c41a0d64952bcaf610e2896186085db
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-03 23:16:31 +0300

    adding crud process

--- ResumeProject/Controllers/SkillController.cs
+++ ResumeProject/Controllers/SkillController.cs
@@ -29,5 +29,33 @@ namespace ResumeProject.Controllers
 
             return RedirectToAction("Index");
         }
-    }
+
+        public ActionResult DeleteSkill(int id)
+        {
+            var value = db.TblSkills.Find(id);
+            db.TblSkills.Remove(value);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateSkill(int id)
+        {
+            var value = db.TblSkills.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateSkill(TblSkill skill)
+        {
+            var value = db.TblSkills.Find(skill.SkillID);
+            value.SkillTitle = skill.SkillTitle;
+            value.SkillIcon = skill.SkillIcon;
+            value.SkillDescription = skill.SkillDescription;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
 }
