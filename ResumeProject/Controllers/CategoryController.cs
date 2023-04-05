ffc409817f711ef06b474b9effa5c762ff035819
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-05 23:07:57 +0300

    adding category pages

---
+++ ResumeProject/Controllers/CategoryController.cs
@@ -0,0 +1,59 @@
+﻿using ResumeProject.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ResumeProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ResumeDBEntities db = new ResumeDBEntities();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            var values = db.TblCategories.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(TblCategory category)
+        {
+            db.TblCategories.Add(category);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            var value = db.TblCategories.Find(id);
+            db.TblCategories.Remove(value);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateCategory(int id)
+        {
+            var value = db.TblCategories.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCategory(TblCategory category)
+        {
+            var value = db.TblCategories.Find(category.CategoryID);
+            value.CategoryName = category.CategoryName;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
