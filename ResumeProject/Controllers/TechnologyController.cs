08f465b0a947944a2e408b1e0975d07dcf64b5f1
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-05 14:11:38 +0300

    adding Technology List

---
+++ ResumeProject/Controllers/TechnologyController.cs
@@ -0,0 +1,18 @@
+﻿using ResumeProject.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ResumeProject.Controllers
+{
+    public class TechnologyController : Controller
+    {
+        ResumeDBEntities db = new ResumeDBEntities();
+
+        // GET: Technology
+        public ActionResult Index()
+        {
+            var values = db.TblTechnologies.ToList();
+            return View(values);
+        }
+    }
+}
