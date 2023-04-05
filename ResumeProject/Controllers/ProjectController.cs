bc9641406a138414b00d9eaa4e15693181f086bc
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-06 00:20:00 +0300

    adding project pages

---
+++ ResumeProject/Controllers/ProjectController.cs
@@ -0,0 +1,61 @@
+﻿using ResumeProject.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ResumeProject.Controllers
+{
+    public class ProjectController : Controller
+    {
+        ResumeDBEntities db = new ResumeDBEntities();
+
+        // GET: Project
+        public ActionResult Index()
+        {
+            var values = db.TblProjects.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddProject()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProject(TblProject project)
+        {
+            db.TblProjects.Add(project);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteProject(int id)
+        {
+            var value = db.TblProjects.Find(id);
+            db.TblProjects.Remove(value);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateProject(int id)
+        {
+            var value = db.TblProjects.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateProject(TblProject project)
+        {
+            var value = db.TblProjects.Find(project.ProjectID);
+            value.ProjectTitle = project.ProjectTitle;
+            value.ProjectDescription = project.ProjectDescription;
+            value.ProjectImageUrl = project.ProjectImageUrl;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
