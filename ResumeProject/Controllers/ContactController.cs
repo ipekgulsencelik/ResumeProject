bab9abcdcb5a7579a7df5abe57f0eb1f3d09ce2f
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-06 00:22:03 +0300

    adding contact list

---
+++ ResumeProject/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using ResumeProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResumeProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        ResumeDBEntities db = new ResumeDBEntities();
+
+        // GET: Contact
+        public ActionResult Index()
+        {
+            var values = db.TblContacts.ToList();
+            return View(values);
+        }
+    }
+}
