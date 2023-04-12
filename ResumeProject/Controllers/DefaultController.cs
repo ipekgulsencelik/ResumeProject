9ad15f562d0641898ddc6959a4c065e8a47cd05a
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-12 17:22:28 +0300

    create partials

--- ResumeProject/Controllers/DefaultController.cs
+++ ResumeProject/Controllers/DefaultController.cs
@@ -9,5 +9,65 @@ namespace ResumeProject.Controllers
         {
             return View();
         }
+
+        public ActionResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialHeader()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialAbout()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialServices()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialFeaturedSkills()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialChoose()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialStats()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialBanner()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialPortfolio()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialBrands()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public ActionResult PartialScripts()
+        {
+            return PartialView();
+        }
     }
 }
