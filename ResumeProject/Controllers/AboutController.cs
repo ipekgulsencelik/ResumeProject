0ea04ce5300bff25b27cc727bb614327024500f6
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-15 02:50:17 +0300

    adding about page

---
+++ ResumeProject/Controllers/AboutController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResumeProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialFeature()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialAbout()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialVideoPopup()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialJoin()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialTestimonial()
+        {
+            return PartialView();
+        }
+    }
+}
